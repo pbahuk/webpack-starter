e463a706ad9249acce148d90326ca7aec470b331
PRABHAT BAHUKHANDI <prabhat.bahukhandi@PRABHATs-MacBook-Pro.local>
2019-08-20 07:40:06 +0530

    Adding constants COLORS, FONT_SIZE, postcss-loader (postcss-constants)

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const COLORS = require("./src/resources/colors");
+const { FONT_SIZE, FONT_WEIGHT } = require("./src/resources/variables");
+
 module.exports = {
-  plugins: [require("autoprefixer")]
+  plugins: [
+    require("autoprefixer"),
+    require("postcss-constants")({
+      defaults: {
+        colors: COLORS,
+        fontSize: FONT_SIZE,
+        fontWeight: FONT_WEIGHT
+      }
+    })
+  ]
 };
---
+++ src/resources/variables.js
@@ -0,0 +1,24 @@
+const FONT_SIZE = {
+  tiny: "8px",
+  tiny_xl: "10px",
+  small: "12px",
+  medium: "14px",
+  icon_large: "22px",
+  large: "16px",
+  large_xl: "18px",
+  xl: "20px",
+  icon: "13px",
+  icon_small: "11px",
+  icon_x_large: "24px",
+  enlarged: "25px"
+};
+
+const FONT_WEIGHT = {
+  light: "300",
+  bold: "600"
+};
+
+module.exports = {
+  FONT_SIZE,
+  FONT_WEIGHT
+};

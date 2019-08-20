ae335a70ef24f165ab6f3a448b187ab2a8c803ea
PRABHAT BAHUKHANDI <prabhat.bahukhandi@PRABHATs-MacBook-Pro.local>
2019-08-20 07:21:43 +0530

    Exctracting the CSS to a different file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -22,7 +23,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
   },
@@ -35,6 +36,9 @@ module.exports = {
         removeComments: true,
         collapseWhitespace: false
       }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
     })
   ]
 };

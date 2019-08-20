f91366443bb498ee38f6591815a370506279bca3
PRABHAT BAHUKHANDI <prabhat.bahukhandi@PRABHATs-MacBook-Pro.local>
2019-08-20 07:04:23 +0530

    Css loader and Style Loader: Basic CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Header from "./app/header";
+import Styles from "./style.css";
 
 const header = new Header();
-console.log("this is index.js", header.random());
+console.log("this is index.js", header.random(), Styles);

318d1cc1e83e37127b88b52ddae19d9b43ee3e28
PRABHAT BAHUKHANDI <prabhat.bahukhandi@PRABHATs-MacBook-Pro.local>
2019-08-20 10:07:23 +0530

    PROD build, optimizations

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,3 +1,9 @@
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = {
-  mode: "production"
+  mode: "production",
+  optimization: {
+    minimizer: [new UglifyJSPlugin(), new OptimizeCSSAssetsPlugin()]
+  }
 };
